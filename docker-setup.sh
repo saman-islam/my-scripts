2e423ad54539d934e4deb36e4ca6fd5c67c0388d
saman-islam <77587371+saman-islam@users.noreply.github.com>
2022-03-29 17:31:07 +0200

    Create docker-setup.sh

---
+++ docker-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+docker ps
+sudo systemctl enable docker
